d19b39a77a438c85d3b7979dad620e667ef528ff
Volodymyr Tsurko <chegnenado@yahoo.com>
2020-03-21 16:23:40 +0200

    Created Utilities library.

---
+++ Advice8.linq
@@ -0,0 +1,4 @@
+<Query Kind="Statements" />
+
+var rand = new Random();
+var sequence = from x in Utilities.Generator
---
+++ Utilities/Utilities.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MoreEffectiveCS
+{
+    public static class Utilities
+    {
+        public static IEnumerable<TResult> Generate<TResult> (int number, Func<TResult> generator)
+        {
+            for (int i = 0; i < number; i++)
+            {
+                yield return generator();
+            }
+        }
+
+        public static IEnumerable<TOutput> Merge<T1, T2, TOutput> (IEnumerable<T1> left, IEnumerable<T2> right, Func<T1, T2, TOutput> generator)
+        {
+            //var pairs = left.Zip(right, (l, r) => new { Left = l, Right = r });
+            //foreach (var p in pairs)
+            //{
+            //    yield return generator(p.Left, p.Right);
+            //}
+            IEnumerator<T1> leftSequence = left.GetEnumerator();
+            IEnumerator<T2> rightSequence = right.GetEnumerator();
+
+            while(leftSequence.MoveNext() && rightSequence.MoveNext())
+            {
+                yield return generator(leftSequence.Current, rightSequence.Current);
+            }
+        }
+    }
+}
